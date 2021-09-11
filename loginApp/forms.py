a4fcb0d66319c2e4938b1246b2b2e665fddbbd86
Nahidjc <nahidjc80@gmail.com>
2021-09-11 21:02:01 +0600

    app settings updated

---
+++ loginApp/forms.py
@@ -0,0 +1,9 @@
+from django.contrib.auth.models import User
+from django import forms
+from django.contrib.auth.forms import AuthenticationForm
+
+
+class UserLoginForm(AuthenticationForm):
+    class Meta:
+        model = User
+        fields = ('username', 'password',)
--- loginApp/views.py
+++ loginApp/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
-
+from loginApp.forms import UserLoginForm
+from django.contrib.auth import authenticate, login, logout
 # Create your views here.
+
+
+def adminLogin(request):
+    form = UserLoginForm()
+    if request.method == 'POST':
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            user = authenticate(username, password)
+            if user is not None:
+                login(request, user)
+            # A backend authenticated the credentials
+
+    return render(request, 'loginApp/login.html', context={'form': form})
+
+
+def logoutUser(request):
+    logout(request)
