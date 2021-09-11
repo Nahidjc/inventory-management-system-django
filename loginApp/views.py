8f48a2d6f14a913f7b21a51d1920b0d6819e2d96
Nahidjc <nahidjc80@gmail.com>
2021-09-11 22:53:26 +0600

    login successfully

--- loginApp/views.py
+++ loginApp/views.py
@@ -1,21 +1,30 @@
 from django.shortcuts import render
 from loginApp.forms import UserLoginForm
 from django.contrib.auth import authenticate, login, logout
+from django.shortcuts import redirect
 # Create your views here.
 
 
 def adminLogin(request):
     form = UserLoginForm()
+    error = ''
     if request.method == 'POST':
+        form = UserLoginForm(data=request.POST)
         if form.is_valid():
             username = form.cleaned_data['username']
             password = form.cleaned_data['password']
-            user = authenticate(username, password)
+            user = authenticate(request, username=username, password=password)
+            print(username, password)
             if user is not None:
                 login(request, user)
+                return redirect('/')
+            else:
+                error = 'Invalid username or password'
             # A backend authenticated the credentials
+        else:
+            error = 'Invalid username or password'
 
-    return render(request, 'loginApp/login.html', context={'form': form})
+    return render(request, 'loginApp/login.html', context={'form': form, 'error': error})
 
 
 def logoutUser(request):
