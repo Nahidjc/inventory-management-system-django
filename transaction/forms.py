bab357d85b55b8b25f184f3ea965ff6e14774b75
Nahidjc <nahidjc80@gmail.com>
2021-09-12 19:47:22 +0600

    model for purchase item

---
+++ transaction/forms.py
@@ -0,0 +1,9 @@
+from django import forms
+from .models import PurchaseItem
+
+
+class PurchaseItemForm(forms.ModelForm):
+
+    class Meta:
+        model = PurchaseItem
+        fields = '__all__'
--- transaction/models.py
+++ transaction/models.py
@@ -1,3 +1,17 @@
 from django.db import models
-
+from inventory.models import inventory, createSupplier
 # Create your models here.
+
+
+class PurchaseItem(models.Model):
+    bill_id = models.AutoField(primary_key=True)
+    supplier = models.ForeignKey(
+        createSupplier, on_delete=models.CASCADE, related_name='purchasesupplier')
+    inventoryName = models.ForeignKey(inventory, on_delete=models.CASCADE)
+    date = models.DateTimeField(auto_now_add=True)
+    itemPrice = models.PositiveIntegerField(default=1)
+    quantity = models.IntegerField(default=1)
+    totalPrice = models.IntegerField(default=1)
+
+    def __str__(self):
+        return self.inventoryName + ' ' + self.quantity
