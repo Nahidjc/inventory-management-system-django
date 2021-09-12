d7685118f83547385924309a41b9a1b6dd9fddf0
Nahidjc <nahidjc80@gmail.com>
2021-09-12 15:05:46 +0600

    supplier model created

--- inventory/admin.py
+++ inventory/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import inventory
+from .models import inventory, createSupplier
 # Register your models here.
 
 admin.site.register(inventory)
+admin.site.register(createSupplier)
--- inventory/models.py
+++ inventory/models.py
@@ -9,3 +9,15 @@ class inventory(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class createSupplier(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=250, unique=True)
+    phone = models.CharField(max_length=250)
+    email = models.EmailField(max_length=100, blank=True)
+    address = models.TextField(blank=True)
+    gstinNo = models.CharField(blank=True, unique=True, max_length=250)
+
+    def __str__(self):
+        self.name
